fe06b3ac994ea95347c07d449c525b65bdbbf177
musrafa <dsdnmstf@gmail.com>
2022-03-30 21:12:23 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css"
+import Balance from "./components/Balance";
+import  Header  from "./components/Header";
+import IncomeExpenses from "./components/IncomeExpenses";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Balance/>
+      <IncomeExpenses/>
+      
+    </div>
+
+  );
+}
+
+export default App;
